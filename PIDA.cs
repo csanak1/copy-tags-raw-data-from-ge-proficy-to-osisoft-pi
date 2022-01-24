867488ee88bbff4006b06f21a240c872edd7b1e2
Sándor Csanak <csasa94@gmail.com>
2022-01-24 14:28:37 +0100

    Data process changed, added new function to delete test values

--- PIDA.cs
+++ PIDA.cs
@@ -85,7 +85,11 @@ namespace CopyTagsRawDataFromGEProficyToOsisoftPI
 
                         if (afObject is AFAttribute afAttribute)
                         {
-                            var afValue = new AFValue(afAttribute, value["Value"], afTimeStamp);
+                            var afValue = new AFValue(afAttribute, value["Value"], afTimeStamp)
+                            {
+                                //AFValueStatus enum doc: https://docs.osisoft.com/bundle/af-sdk/page/html/T_OSIsoft_AF_Asset_AFValueStatus.htm
+                                Status = (string)value["Quality"] == "Bad" ? AFValueStatus.Bad : AFValueStatus.Good
+                            };
 
                             valuesToWrite.Add(afValue);
                         }
@@ -108,7 +112,7 @@ namespace CopyTagsRawDataFromGEProficyToOsisoftPI
                         if (errors != null)
                         {
                             //TODO: handle errors
-                            Console.WriteLine("An error occured during inserting AFValues.");
+                            Console.WriteLine("!!! An error occured during inserting AFValues.");
 
                             return false;
                         }
@@ -120,7 +124,7 @@ namespace CopyTagsRawDataFromGEProficyToOsisoftPI
                 }
                 else return false;
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine("Error at inserting values into PI systems: " + e.Message);
 
@@ -131,6 +135,236 @@ namespace CopyTagsRawDataFromGEProficyToOsisoftPI
                 Disconnect();
             }
         }
+
+        public bool UpdateTagValues(string tagName, DataTable values, bool replace)
+        {
+            Connect();
+
+            try
+            {
+                var piTag = FindTagPIPointIfExists(tagName);
+
+                if (piTag != null)
+                {
+                    string piPointPath = $@"\\PIServer[{_piServerName}]\PIPoint[{piTag.Name}]";
+
+                    //AFObject.FindObject Method doc: https://docs.osisoft.com/bundle/af-sdk/page/html/M_OSIsoft_AF_AFObject_FindObject_2.htm
+                    var afObject = AFObject.FindObject(piPointPath, piDatabase);
+
+                    //AFUpdateOption enum doc: https://docs.osisoft.com/bundle/af-sdk/page/html/T_OSIsoft_AF_Data_AFUpdateOption.htm
+                    var afInsertMode = AFUpdateOption.Replace; //Add the value to the archive. If any values exist at the same time, will overwrite one of them and set its Substituted flag.
+                    //var afInsertMode = AFUpdateOption.NoReplace; //Add the value to the archive only if no value exists at the same time. If a value already exists for that time, the passed value is ignored.
+
+                    if (!replace)
+                    {
+                        afInsertMode = AFUpdateOption.ReplaceOnly; //Replace an existing value in the archive at the specified time. If no existing value is found, the passed value is ignored.
+                    }
+
+                    //AFBufferOption enum doc: https://docs.osisoft.com/bundle/af-sdk/page/html/T_OSIsoft_AF_Data_AFBufferOption.htm
+                    var afBuffer = AFBufferOption.BufferIfPossible;
+
+                    //AFValue Constructor (AFValue) doc: https://docs.osisoft.com/bundle/af-sdk/page/html/M_OSIsoft_AF_Asset_AFValue__ctor_4.htm
+                    var valuesToWrite = new List<AFValue>();
+
+                    foreach (DataRow value in values.Rows)
+                    {
+                        var afTimeStamp = new AFTime("*");
+
+                        if ((DateTime)value["TimeStamp"] != DateTime.MinValue || (DateTime)value["TimeStamp"] <= DateTime.Now) //default -> set AFTime to DateTime.Now
+                        {
+                            afTimeStamp = new AFTime((DateTime)value["TimeStamp"]);
+                        }
+
+                        if (afObject is AFAttribute afAttribute)
+                        {
+                            var afValue = new AFValue(afAttribute, value["Value"], afTimeStamp)
+                            {
+                                //AFValueStatus enum doc: https://docs.osisoft.com/bundle/af-sdk/page/html/T_OSIsoft_AF_Asset_AFValueStatus.htm
+                                Status = (string)value["Quality"] == "Bad" ? AFValueStatus.Bad : AFValueStatus.Good
+                            };
+
+                            valuesToWrite.Add(afValue);
+                        }
+                        else
+                        {
+                            valuesToWrite.Clear();
+
+                            break;
+                        }
+                    }
+
+                    if (valuesToWrite.Count > 0)
+                    {
+                        //Perform a bulk write. Use a single local call to PI Buffer Subsystem if possible.
+                        //AFListData is the class that provides the bulk write method.
+                        //AFListData.UpdateValues method doc: https://docs.osisoft.com/bundle/af-sdk/page/html/M_OSIsoft_AF_Data_AFListData_UpdateValues_1.htm
+                        var errors = AFListData.UpdateValues(valuesToWrite, afInsertMode, afBuffer);
+                        //AFListData.UpdateValues method returns AFErrors<AFValue>: https://docs.osisoft.com/bundle/af-sdk/page/html/T_OSIsoft_AF_AFErrors_1.htm
+
+                        if (errors != null)
+                        {
+                            //TODO: handle errors
+                            Console.WriteLine("!!! An error occured during updating AFValues.");
+
+                            return false;
+                        }
+
+                        return true;
+                    }
+                    else return false;
+
+                }
+                else return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error at inserting values into PI systems: " + e.Message);
+
+                return false;
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+
+        public bool DeleteTagValues(AFValues values)
+        {
+            Connect();
+
+            try
+            {
+                //AFUpdateOption enum doc: https://docs.osisoft.com/bundle/af-sdk/page/html/T_OSIsoft_AF_Data_AFUpdateOption.htm
+                var afInsertMode = AFUpdateOption.Remove; //Remove the value from the archive if a value exists at the passed time.
+
+                //AFBufferOption enum doc: https://docs.osisoft.com/bundle/af-sdk/page/html/T_OSIsoft_AF_Data_AFBufferOption.htm
+                var afBuffer = AFBufferOption.BufferIfPossible;
+
+                //AFValue Constructor (AFValue) doc: https://docs.osisoft.com/bundle/af-sdk/page/html/M_OSIsoft_AF_Asset_AFValue__ctor_4.htm
+                var valuesToWrite = new List<AFValue>();
+
+                foreach (var afValue in values)
+                {
+                    valuesToWrite.Add(afValue);
+                }
+
+                if (valuesToWrite.Count > 0)
+                {
+                    //Perform a bulk write. Use a single local call to PI Buffer Subsystem if possible.
+                    //AFListData is the class that provides the bulk write method.
+                    //AFListData.UpdateValues method doc: https://docs.osisoft.com/bundle/af-sdk/page/html/M_OSIsoft_AF_Data_AFListData_UpdateValues_1.htm
+                    var errors = AFListData.UpdateValues(valuesToWrite, afInsertMode, afBuffer);
+                    //AFListData.UpdateValues method returns AFErrors<AFValue>: https://docs.osisoft.com/bundle/af-sdk/page/html/T_OSIsoft_AF_AFErrors_1.htm
+
+                    if (errors != null)
+                    {
+                        //TODO: handle errors
+                        Console.WriteLine("!!! An error occured during removing AFValues.");
+
+                        return false;
+                    }
+
+                    return true;
+                }
+                else return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error at inserting values into PI systems: " + e.Message);
+
+                return false;
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+
+        public AFValues QueryTagRawValues(string tagName, DateTime start, DateTime end)
+        {
+            var afValues = new AFValues();
+
+            Connect();
+
+            var afStart = new AFTime(start);
+            var afEnd = new AFTime(end);
+            var afTimeRange = new AFTimeRange(afStart, afEnd);
+
+            //AFBoundaryType enumeration doc: https://docs.osisoft.com/bundle/af-sdk/page/html/T_OSIsoft_AF_Data_AFBoundaryType.htm
+            var afBoundatyType = AFBoundaryType.Inside; //query points inside start and endtime
+
+            var piTag = PIPoint.FindPIPoint(piServer, tagName);
+
+            string piPointPath = $@"\\PIServer[{_piServerName}]\PIPoint[{piTag.Name}]";
+
+            var afObject = AFObject.FindObject(piPointPath, piDatabase);
+
+            if (afObject is AFAttribute afAttribute)
+            {
+                //AFBoundaryType enumeration doc: https://docs.osisoft.com/bundle/af-sdk/page/html/T_OSIsoft_AF_Data_AFBoundaryType.htm
+                //AFData.RecordedValues method doc: https://docs.osisoft.com/bundle/af-sdk/page/html/M_OSIsoft_AF_Data_AFData_RecordedValue.htm
+                afValues = afAttribute.Data.RecordedValues(
+                    timeRange: afTimeRange,
+                    boundaryType: afBoundatyType,
+                    desiredUOM: null,
+                    filterExpression: null,
+                    includeFilteredValues: false);
+            }
+
+            Disconnect();
+
+            return afValues;
+        }
+
+        public AFValues QueryTagRawValues(string tagName, DateTime start, int valueCount, bool goForward)
+        {
+            var afValues = new AFValues();
+
+            Connect();
+
+            var afStart = new AFTime(start);
+
+            //AFBoundaryType enumeration doc: https://docs.osisoft.com/bundle/af-sdk/page/html/T_OSIsoft_AF_Data_AFBoundaryType.htm
+            var afBoundatyType = AFBoundaryType.Inside; //query points inside start and endtime
+
+            var piTag = PIPoint.FindPIPoint(piServer, tagName);
+
+            string piPointPath = $@"\\PIServer[{_piServerName}]\PIPoint[{piTag.Name}]";
+
+            var afObject = AFObject.FindObject(piPointPath, piDatabase);
+
+            if (afObject is AFAttribute afAttribute)
+            {
+                //AFData.RecordedValuesByCount method doc: https://docs.osisoft.com/bundle/af-sdk/page/html/M_OSIsoft_AF_Data_AFData_RecordedValuesByCount.htm
+                afValues = afAttribute.Data.RecordedValuesByCount(
+                    startTime: afStart,
+                    count: valueCount,
+                    boundaryType: afBoundatyType,
+                    forward: goForward,
+                    desiredUOM: null,
+                    filterExpression: null,
+                    includeFilteredValues: false);
+            }
+
+            Disconnect();
+
+            return afValues;
+        }
+
+        //private void PrintAFValues(AFValues afValues)
+        //{
+        //    if (afValues != null)
+        //    {
+        //        foreach (var afValue in afValues)
+        //        {
+        //            Console.WriteLine("Tag: {0}, Timestamp (Local): {1}, Value: {2}, Quality: {3}", afValue.PIPoint, afValue.Timestamp.LocalTime, GetStringValue(afValue), afValue.IsGood.ToString());
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Query returned result of NULL. Check Tag and query syntax!");
+        //    }
+        //}
 
         private void Connect()
         {
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace CopyTagsRawDataFromGEProficyToOsisoftPI
     class Program
     {
         private static readonly HistDA histDA = new HistDA();
-        private static readonly PIDA pida = new PIDA();
+        private static readonly PIDA piDA = new PIDA();
 
         static void Main(string[] args)
         {
@@ -20,14 +20,18 @@ namespace CopyTagsRawDataFromGEProficyToOsisoftPI
             Console.WriteLine("At this point this project is for test purposes only!");
             Console.WriteLine("");
 
-            Start();
+            StartDataProcessFromProficyHistorian();
+            //DeleteValuesFromPISystem();
+
+            Console.ReadKey();
         }
 
-        private static void Start()
+        private static void StartDataProcessFromProficyHistorian()
         {
             //string piTag = "BC.M.MANUAL_TEST.PV";     //Float32 test Tag in PI:   BC.M.MANUAL_TEST.PV
             //string piTag = "BC.M.MANUAL_TEST2.PV";    //String test Tag in PI:    BC.M.MANUAL_TEST2.PV
             //string piTag = "BC.M.MANUAL_TEST3.PV";    //Digital test Tag in PI:   BC.M.MANUAL_TEST3.PV -> for motors/pumps/valves open-close positions
+            //string piTag = "BC.M.MANUAL_TEST4.PV";    //Int16 test Tag in PI:   BC.M.MANUAL_TEST4.PV
 
             //Data Type:                                       GE Proficy       ->      Osisoft PI
             //string tagName = "BC.TDI1.FIC3401.PV";        // Single Float     ->      Float32 
@@ -37,14 +41,15 @@ namespace CopyTagsRawDataFromGEProficyToOsisoftPI
             //string tagName = "BC.TDI2.UP6901A.PV";        // Single Integer   ->      Digital (running signal:    0;2 -> 0 is not running always)
             //string tagName = "BC.HCLCONV.XV1700B.PV";     // Single Float     ->      Digital (running signal:    -1;0 -> 0 is not running always)
 
-            string[] tagNames = { "BC.TDI1.FIC3401.PV", "BC.TDI1.FIC3401.MODE", "BC.HCLCONV.FIC1020.MODE", "BC.TDI1.P3774BFUT.PV", "BC.TDI2.UP6901A.PV", "BC.HCLCONV.XV1700B.PV" };
+            string[] tagNames = { "BC.M.PITEST1.PV", "BC.M.PITEST2.PV", "BC.M.PITEST3.PV" };
+            //string[] tagNames = { "BC.TDI1.FIC3401.PV" };
 
-            var startDate = new DateTime(2021, 01, 01, 00, 00, 00); // give a correct start date
+            var startDate = new DateTime(2022, 01, 01, 00, 00, 00); // give a correct start date
             //var startDate = DateTime.Now.AddHours(-12);
             //var startDate = DateTime.Now.AddDays(-120);
 
-            var endDate = new DateTime(2022, 01, 01, 00, 00, 00); // give a correct end date
-            //var endDate = DateTime.Now;
+            //var endDate = new DateTime(2022, 01, 01, 00, 00, 00); // give a correct end date
+            var endDate = DateTime.Now;
 
             foreach (string tagName in tagNames)
             {
@@ -56,22 +61,25 @@ namespace CopyTagsRawDataFromGEProficyToOsisoftPI
 
                 var dataToImport = rawData.AsEnumerable()
                     .Where(r => r.Field<DateTime>("TimeStamp") <= endDate)
-                    .Where(r => r.Field<string>("Quality") == "Good")
+                    //.Where(r => r.Field<string>("Quality") == "Good")
                     .Select
                     (r => new
                     {
                         Value = r.Field<string>("Value"),
+                        Quality = r.Field<string>("Quality"),
                         TimeStamp = r.Field<DateTime>("TimeStamp").ToLocalTime().IsDaylightSavingTime() ?
                             r.Field<DateTime>("TimeStamp").ToLocalTime() :
                             r.Field<DateTime>("TimeStamp").ToLocalTime().AddHours(-1) //UTC -> CET - CEST correction!!!
-                }).ToList();
+                    }).ToList();
 
                 if (dataToImport.Count > 0)
                 {
                     Console.WriteLine("Start inserting values into AF database!");
 
-                    //bool result = pida.InsertTagValues(piTag, Extensions.ConvertToDataTable(dataToImport), false); //for TEST Tags, MUST BE added manually
-                    bool result = pida.InsertTagValues(tagName, Extensions.ConvertToDataTable(dataToImport), false); 
+                    //bool result = piDA.UpdateTagValues(piTag, Extensions.ConvertToDataTable(dataToImport), true); //for TEST Tags, MUST BE added manually
+                    //bool result = piDA.UpdateTagValues(tagName, Extensions.ConvertToDataTable(dataToImport), false);
+                    //bool result = piDA.InsertTagValues(piTag, Extensions.ConvertToDataTable(dataToImport), false);
+                    bool result = piDA.InsertTagValues(tagName, Extensions.ConvertToDataTable(dataToImport), false);
 
                     if (result)
                     {
@@ -85,7 +93,6 @@ namespace CopyTagsRawDataFromGEProficyToOsisoftPI
             }
 
             Console.WriteLine("Finished all tags.");
-            Console.ReadKey();
         }
 
         private static DataTable ProcessDataFromProficy(string tagName, DateTime startDate, DateTime endDate)
@@ -137,6 +144,40 @@ namespace CopyTagsRawDataFromGEProficyToOsisoftPI
             }
 
             return tagRawData;
+        }
+
+        private static void DeleteValuesFromPISystem()
+        {
+            string piTag = "BC.M.MANUAL_TEST3.PV";     //Float32 test Tag in PI:   BC.M.MANUAL_TEST.PV
+
+            Console.WriteLine("Delete method started for tag: " + piTag);
+
+            var startDate = new DateTime(2021, 01, 18, 12, 52, 50, DateTimeKind.Local); // give a correct start date
+            var endDate = new DateTime(2022, 01, 19, 13, 52, 50, DateTimeKind.Local); // give a correct end date
+
+            Console.WriteLine("Delete start: " + startDate.ToString("yyyy.MM.dd. HH:mm:ss"));
+            Console.WriteLine("Delete end: " + endDate.ToString("yyyy.MM.dd. HH:mm:ss"));
+
+            var values = piDA.QueryTagRawValues(piTag, startDate, endDate);
+            //var values = piDA.QueryTagRawValues(piTag, startDate, 1, true);
+
+            if (values.Count != 0)
+            {
+                var result = piDA.DeleteTagValues(values);
+
+                if (result)
+                {
+                    Console.WriteLine(values.Count + " value(s) are deleted!");
+                }
+                else
+                {
+                    Console.WriteLine("No values are deleted!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data can be deleted for the selected interval!");
+            }
         }
     }
 }
